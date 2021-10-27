9c8a86511b295d596b8fdeccc7f792ca32735fdb
soobinism <kibinsoo@gmail.com>
2021-10-27 13:56:56 -0400

    Adjusted look of app

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -21,18 +21,13 @@ class MainActivity : AppCompatActivity(), BookListFragment.EventInterface  {
 
         val bookListFragment = BookListFragment.newInstance(booksList)
 
-        // Check if BookDetailsFragment is open in portrait mode. If so, pop of the stack before
-        // landscape orientation change.
         if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView1) is BookDetailsFragment
             && isTwoPane) {
             supportFragmentManager.popBackStack()
         }
 
-        // Check if BookDetailsFragment is occupied with data in landscape and
-        // open in portrait orientation change.
         if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) is BookDetailsFragment
             && !isTwoPane) {
-            // Only open details over top of portrait recycler view if they are not empty.
             if (ViewModelProvider(this).get(BookViewModel::class.java).getBook().value?.title != ""
                 && !bookViewModel.isEmpty()) {
                 selectionMade()
@@ -40,14 +35,12 @@ class MainActivity : AppCompatActivity(), BookListFragment.EventInterface  {
 
         }
 
-        // Attach book list fragment only when app first starts.
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView1, bookListFragment)
                 .commit()
         }
 
-        // If it is two panes attach second fragment, book details.
         if(isTwoPane){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView2, BookDetailsFragment())
@@ -56,13 +49,6 @@ class MainActivity : AppCompatActivity(), BookListFragment.EventInterface  {
 
     }
 
-    /**
-     * The populateBooks function will fill the BookList with the same number of books
-     * provided from the parameters of the function.
-     *
-     * @param numOfBooks The number of books to populate the BookList with.
-     * @param booksList The BookList to populate.
-     */
     private fun populateBooks(numOfBooks: Int, booksList: BookList) {
 
         for (i in 0..numOfBooks) {
@@ -72,15 +58,12 @@ class MainActivity : AppCompatActivity(), BookListFragment.EventInterface  {
     }
 
     override fun selectionMade() {
-        // If selection is made and it is not two pane open over top of book details fragment.
         if (!isTwoPane) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView1, BookDetailsFragment())
                 .addToBackStack(null)
                 .commit()
         }
-        // If the device is in two pane mode and back is pressed to remove details reattach
-        // fragment if needed.
         else{
             if(supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) == null) {
                 supportFragmentManager.beginTransaction()
