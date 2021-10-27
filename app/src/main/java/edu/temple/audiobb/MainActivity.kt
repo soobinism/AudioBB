c442be6bb276bb1c13aabb110e642c25df39286a
kibinsoo@gmail.com <44609168+soobinism@users.noreply.github.com>
2021-10-27 00:27:16 -0400

    populateBooks function created to populate Book List

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -2,10 +2,27 @@ package edu.temple.audiobb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val booksList = BookList()
+
+        populateBooks(10, booksList)
+
+        for (i in 0 until booksList.size()) {
+            Log.d("title list", booksList.get(i).title)
+            Log.d("author list", booksList.get(i).author)
+        }
+    }
+
+    private fun populateBooks(numberOfBooks : Int, booksList : BookList){
+        for (i in 0..numberOfBooks){
+            val book = Book("Title: $i", "Author: $i")
+            booksList.add(book)
+        }
     }
 }
