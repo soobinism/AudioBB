8f4413b949055eaace73d805ea036d77fcb54a4c
kibinsoo@gmail.com <44609168+soobinism@users.noreply.github.com>
2021-11-08 20:53:56 -0500

    Added id and coverURL fields to Book class

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -1,4 +1,4 @@
 package edu.temple.audiobb
 
-data class Book (val title: String, val author: String){
+data class Book (val title: String, val author: String, val id: Int, val coverURL: String){
 }
