776663f2cc886a8f9ff9ae1c9809b0de69f238c7
kibinsoo@gmail.com <44609168+soobinism@users.noreply.github.com>
2021-11-27 08:16:52 -0500

    Add duration field to Book class

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -2,5 +2,5 @@ package edu.temple.audiobb
 
 import java.io.Serializable
 
-data class Book(val id: Int, val title: String, val author: String, val coverURL: String): Serializable{
+data class Book(val id: Int, val title: String, val author: String, val coverURL: String, val duration: Int): Serializable{
 }
