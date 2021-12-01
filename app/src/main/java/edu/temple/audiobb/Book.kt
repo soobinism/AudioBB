5f28764df063dbf56a0d95fd9fa3c19118d11001
kibinsoo@gmail.com <44609168+soobinism@users.noreply.github.com>
2021-12-01 08:44:03 -0500

    ControlFragment and SelectedBookViewModel classes created

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -1,6 +1,8 @@
 package edu.temple.audiobb
 
+import org.json.JSONObject
 import java.io.Serializable
 
 data class Book(val id: Int, val title: String, val author: String, val coverURL: String, val duration: Int): Serializable{
+    constructor(book: JSONObject) : this(book.getInt("id"), book.getString("title"), book.getString("author"), book.getString("cover_url"), book.getInt("duration"))
 }
